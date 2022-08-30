1e09e80ac75b10a8fde857c8aa1561d53f091079
konay812020 <konay812020@gmail.com>
2022-08-30 08:00:45 +0630

    version

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ for (var x = 0; x < getimgs.length; x++) {
     getimgs[x].addEventListener("click", function () {
         shownow(this)
     })
+
 }
 
 
 function shownow(ele) {
     // console.log(ele);
     // console.log(ele.src);
-    console.log(ele.alt)
+    // console.log(ele.alt);
+
+    getmodal.style.display = "block";
+    getmodalimg.src = ele.src;
+    getcaption.textContent = ele.alt;
 }
